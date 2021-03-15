79d107c6d828c8faad212b9d0d44bd884ea3237b
Alex Seiger <awseiger@gmail.com>
2021-03-15 11:05:20 -0400

    Initial thermistor testing

---
+++ I2CCaseController/I2CThermistor.py
@@ -0,0 +1,16 @@
+import board
+import busio
+import math
+from adafruit_ads1x15.analog_in import AnalogIn
+i2c = busio.I2C(board.SCL, board.SDA)
+import adafruit_ads1x15.ads1115 as ADS
+
+ads = ADS.ADS1115(i2c)
+ads.mode = ADS.Mode.SINGLE
+chan = AnalogIn(ads, ADS.P1)
+
+resistance = 10000 / (3.3 / chan.voltage - 1)
+
+temperature = 1/((1/298.15) + (1/4036)*math.log(resistance/100000.0)) - 273.15
+
+print(chan.value, chan.voltage, resistance, temperature)
